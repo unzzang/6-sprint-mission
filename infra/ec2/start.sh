6bd57f8eaa454689f34bdad137a445740486e7ff
이상운 <unzzang@gmail.com>
2026-02-13 14:40:37 +0900

    chore:start.sh 파일 수정

--- infra/ec2/start.sh
+++ infra/ec2/start.sh
@@ -2,11 +2,13 @@
 pm2 delete "panda-market-api" 2>/dev/null || true
 
 #앱실행
-pm2 start dist/main.js --name "panda-market-api"
+# pm2 start dist/main.js --name "panda-market-api"
+pm2 start ecosystem.config.js --env production
 
 # 현재상태 저장
 pm2 save
 
+echo "PM2 실행 및 저장 완료! 'pm2 startup' 명령은 직접 터미널에서 실행해주세요!"
 #-------
 #앱 자동 실행
 #pm2 startup 
